15da4debc381a3bc4d9ed0c8843100416fae50c1
noobmaster69jr <srashwin22@gmail.com>
2023-12-12 23:23:40 +0530

    easy 1

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,10 @@
 */
 
 function isAnagram(str1, str2) {
-
+  if(str1.length != str2.length) return false;
+  let new_arr1 = str1.toLowerCase().split("").sort().join("")
+  let new_arr2 = str2.toLowerCase().split("").sort().join("")
+  return new_arr1 === new_arr2
 }
 
 module.exports = isAnagram;

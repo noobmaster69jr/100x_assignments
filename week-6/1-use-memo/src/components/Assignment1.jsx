334d347000792147373e04228de32f731bd257aa
noobmaster69jr <srashwin22@gmail.com>
2024-01-09 17:05:22 +0530

    use-memo caches the result of a calculation betweeen re-render

--- week-6/1-use-memo/src/components/Assignment1.jsx
+++ week-6/1-use-memo/src/components/Assignment1.jsx
@@ -1,14 +1,20 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 
 // In this assignment, your task is to create a component that performs an expensive calculation (finding the factorial) based on a user input. 
 // Use useMemo to ensure that the calculation is only recomputed when the input changes, not on every render.
-
+ 
 export function Assignment1() {
     const [input, setInput] = useState(0);
     // Your solution starts here
     const expensiveValue = 0; 
     // Your solution ends here
-
+     console.log('rendered');
+    const expensiveOperation = () => {
+        console.log("expensive operation")
+        return 23;
+    }
+    const value = useMemo(expensiveOperation, [])
+    console.log(value)
     return (
         <div>
             <input 
